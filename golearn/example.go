3b7ce96f370ab47c5f8cb310cee65dca563b881c
lazuardii8 <ardiaspal97@gmail.com>
2020-10-13 10:54:21 +0700

    add init

---
+++ golearn/example.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+// variable
+var pelajaran1 = "GO"
+var pelajaran2 = "PHP"
+
+func main(){
+	fmt.Println("hallo semuanya saya sedang belajar bahasa " + pelajaran1 + " dan " + pelajaran2)
+}
